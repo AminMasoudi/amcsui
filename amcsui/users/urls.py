8ad99f87a2e78923c58e3aecd51a34c28b9f81a1
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-04-13 12:32:04 +0330

    some login bogs fixed

--- amcsui/users/urls.py
+++ amcsui/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path("", views.index, name="user/index"),
+    path("", views.index, name="index"),
     path("login_view", views.login_view, name="login_view"),
     path("signup", views.signup, name="sign_up"),
 ]
