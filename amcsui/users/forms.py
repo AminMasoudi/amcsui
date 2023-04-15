ca5fd9a444512acce5f8f28a21e6d150bfb0a554
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-04-15 08:02:47 +0330

    cleaning code part 1.1

--- amcsui/users/forms.py
+++ amcsui/users/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login
-
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65, required=True)
@@ -15,10 +16,30 @@ class LoginForm(forms.Form):
             return user
         return False
 
-class Signup(forms.Form):
-    username = forms.CharField(max_length=65, required=True)
-    password = forms.CharField(max_length=65, required=True,widget=forms.PasswordInput)
+
+
 
 class Registeration_form(UserCreationForm):
     username = forms.CharField(max_length=65)
     email = forms.EmailField()
+
+    def is_unique_username(self):
+        if self.is_valid():    
+            user = User.objects.filter(username=self.cleaned_data["username"])
+            if user:
+                return False
+            return True
+        return False
+
+
+    def auth(self):
+        if self.is_unique_username():
+            user = User.objects.create_user(username=self.cleaned_data["username"],
+                                            email=self.cleaned_data["email"],
+                                            password=self.clean_password2())
+            profile = UserProfile.objects.create(user=user,
+                                                 email=user.email)
+            profile.save()
+            return user
+
+        return False
