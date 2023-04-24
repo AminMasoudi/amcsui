818601dd4b2e7fd53ef96cf71b5ca173d1eb9daa
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-04-24 20:48:04 +0330

    more tests

--- amcsui/users/tests.py
+++ amcsui/users/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase, Client
-
+from django.urls import reverse
 from .models import UserProfile, User
 from .forms import LoginForm
 # Create your tests here.
 
+
 class UsersTestCase(TestCase):
+
     def setUp(self) -> None:
         self.username = "test"
         self.password = "whatch0ut"
@@ -13,44 +15,41 @@ class UsersTestCase(TestCase):
 
     def test_index(self):
         c = Client()
-        response = c.get("/users/")
-        self.assertEqual(response.status_code,302)
-
-
-    def test_login_view(self):
-        c = Client()
-        response = c.post("/users/login_view",{"username":self.username,
-                                               "password": self.password})
-
+        response = c.get(reverse("users:index"))
         self.assertEqual(response.status_code, 302)
 
     def test_login_logout(self):
         c = Client()
         c.login(username=self.username, password=self.password)
-        response = c.get("/users/")
-        self.assertEqual(response.status_code,200)
+        response = c.get(reverse("users:index"))
+        self.assertEqual(response.status_code, 200)
         c.logout()
-        response = c.get("/users/")
-        self.assertEqual(response.status_code,302)
+        response = c.get(reverse("users:index"))
+        self.assertEqual(response.status_code, 302)
 
+    def test_login_view(self):
+        c = Client()
+        get_response = c.get(reverse("users:login_view"))
+        post_response = c.post(reverse("users:login_view"), {"username": self.username,
+                                                             "password": self.password})
 
+        self.assertEqual(post_response.status_code, 302)
+        self.assertEqual(get_response.status_code, 200)
 
     def test_sign_up_view(self):
         c = Client()
-        self.password = "whatch0ut"
-        response = c.post("/users/signup",{"username":"test2",
-                                           "email":"a@gg.co",
-                                           "password1":self.password,
-                                           "password2":self.password})     
-        self.assertEqual(response.status_code,302,response.cookies)
-        self.assertEqual(response.url,"/users/")
+        response = c.post(reverse("users:sign_up"), {"username": "test2",
+                                                        "email": "a@gg.co",
+                                                        "password1": self.password,
+                                                        "password2": self.password})     
+        self.assertEqual(response.status_code, 302, response.cookies)
+        self.assertEqual(response.url, reverse("users:index"))
 
     def test_logout(self):
         c = Client()
         c.login(username=self.username, password=self.password)
+        response = c.get(reverse("users:logout"))
+        self.assertEqual(response.status_code, 302, response.cookies)
         
-        c.logout()
-
-#TODO: test logout
-
-#TODO: test froude logins and ...
+    
+# TODO: test froude logins and ...
