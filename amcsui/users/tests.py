bb002c05b8884af7d9c77c80a9785fad284d72b4
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-04-24 15:28:10 +0330

    some test added

--- amcsui/users/tests.py
+++ amcsui/users/tests.py
@@ -6,9 +6,10 @@ from .forms import LoginForm
 
 class UsersTestCase(TestCase):
     def setUp(self) -> None:
-        a = User.objects.create_user(username="test",password="whatch0ut")
+        self.username = "test"
+        self.password = "whatch0ut"
+        a = User.objects.create_user(username=self.username, password=self.password)
         UserProfile.objects.create(user=a)
-
 
     def test_index(self):
         c = Client()
@@ -16,18 +17,40 @@ class UsersTestCase(TestCase):
         self.assertEqual(response.status_code,302)
 
 
-    def test_login(self):
+    def test_login_view(self):
         c = Client()
-        response = c.post("/users/login_view",{"username":"test",
-                                               "password": "whatch0ut"})
+        response = c.post("/users/login_view",{"username":self.username,
+                                               "password": self.password})
+
         self.assertEqual(response.status_code, 302)
 
-    def test_sign_up(self):
+    def test_login_logout(self):
         c = Client()
-        passwords = "whatch0ut"
+        c.login(username=self.username, password=self.password)
+        response = c.get("/users/")
+        self.assertEqual(response.status_code,200)
+        c.logout()
+        response = c.get("/users/")
+        self.assertEqual(response.status_code,302)
+
+
+
+    def test_sign_up_view(self):
+        c = Client()
+        self.password = "whatch0ut"
         response = c.post("/users/signup",{"username":"test2",
                                            "email":"a@gg.co",
-                                           "password1":passwords,
-                                           "password2":passwords})
+                                           "password1":self.password,
+                                           "password2":self.password})     
         self.assertEqual(response.status_code,302,response.cookies)
         self.assertEqual(response.url,"/users/")
+
+    def test_logout(self):
+        c = Client()
+        c.login(username=self.username, password=self.password)
+        
+        c.logout()
+
+#TODO: test logout
+
+#TODO: test froude logins and ...
